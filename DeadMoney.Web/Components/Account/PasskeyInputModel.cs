d0f20c362678528b0d6365f0e47187fd1205bb1d
timcu <timcu@TCULLISON-HOME>
2026-02-09 18:03:18 -0500

    Add project files.

---
+++ DeadMoney.Web/Components/Account/PasskeyInputModel.cs
@@ -0,0 +1,8 @@
+namespace DeadMoney.Web.Components.Account
+{
+    public class PasskeyInputModel
+    {
+        public string? CredentialJson { get; set; }
+        public string? Error { get; set; }
+    }
+}
---
+++ DeadMoney.Web/Components/Account/PasskeyOperation.cs
@@ -0,0 +1,8 @@
+namespace DeadMoney.Web.Components.Account
+{
+    public enum PasskeyOperation
+    {
+        Create = 0,
+        Request = 1,
+    }
+}
